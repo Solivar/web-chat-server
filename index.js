2ea8a536700e447ed52106c0b21b378092fb57eb
Ivars Solovjovs <solovjovs.ivars@gmail.com>
2021-08-20 11:06:54 +0300

    Clean up

--- index.js
+++ index.js
@@ -1,18 +1,8 @@
-const express = require('express');
-const morgan = require('morgan');
-
 const port = 8000;
-const app = express();
 
 const httpServer = require('http').createServer();
 require('./socket')(httpServer);
 
-app.use(morgan('dev'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 httpServer.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App running on http://localhost:${port}`);
 });
